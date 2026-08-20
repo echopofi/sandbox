486ba4ce599be449a10c2c20a7ef15123ee438ae
Echo-007-sudo <bosspofii@gmail.com>
2026-08-20 12:00:23 +0100

    understanding array indexing

--- app.js
+++ app.js
@@ -2,16 +2,43 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+
+const users = [
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@example.com',
+  },
+  {
+    first_name: 'Alice',
+    last_name: 'Smith',
+    email: 'alicesmith@example.com',
+  },
+];
+
 app.get("/", (req, res) => {
 	//res.status(200).json({message: "hello world", status: "succuess"})
 	//res.sendStatus(200).json({message: "hhhhhh"})
 	//res.redirect("http://fb.com")//redirects to an external link
-	res.redirect("/redirection")
+	//res.redirect("/redirection")
+	
+
+	res.json(users)
 })
 
 
 app.get("/redirection", (req, res)=>{
 	res.send("redirected end-point")
+})
+
+app.get("/users/:index", (req, res) =>{
+	const index = Number(req.params.index)
+	const user = users[index]
+
+	if (!user){
+		return res.status(400).json({message: "User not found"})
+	}
+	res.status(200).json(users[index])
 })
 
 app.listen(3001, () =>{
--- loft.js
+++ loft.js
@@ -1,17 +1,34 @@
 const express = require("express")
 const app = express()
-const PORT = 3002
+const PORT = 3003
+const users = [
+  { id: 1, name: "Zacksman", role: "Admin", active: true },
+  { id: 2, name: "Bobby Emma", role: "User", active: false },
+  { id: 3, name: "Charlie", role: "Moderator", active: true },
+  { id: 4, name: "Diana", role: "User", active: true }
+];
+
 app.use(express.json())
 
-app.get("/f", (req, res) =>{
-	res.json({
-		"message": "Hello World",
-		"status":"EP hit",
-		"isActive": true,
-
-	})
+app.get("/", (req, res)=>{
+	res.send("Welcome to my test api")
 })
 
-app.listen(PORT, () =>{
-	console.log("Server PORT 3002")
+app.get("/users", (req, res)=>{
+	res.json(users)
+})
+
+app.get("/users/:index", (req, res)=>{
+	const index = Number(req.params.index)
+	const user = users[index]
+
+	if (!user){
+		return res.json({message: "User not found"})
+	}
+	res.json(user)
+})
+
+
+app.listen(PORT, ()=>{
+	console.log("Server running at Port 3003")
 })
