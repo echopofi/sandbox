c4c627c0964f046963cb13f3ffe30a09f7979d89
Echo-007-sudo <bosspofii@gmail.com>
2026-08-21 00:31:36 +0100

    modified file

--- loft.js
+++ loft.js
@@ -29,6 +29,20 @@ app.get("/users/:index/role", (req, res)=>{
 })
 
 
+app.get("/search", (req,res) => {
+	const role = req.query.role
+	const filtered = users.filter(user => user.role.toLowerCase() === role.toLowerCase())
+
+	if (filtered.length === 0) {
+		return res.status(404).json({ message: "Role not assigned to user" })
+	}
+	res.status(200).json(filtered)
+})
+
+app.get("/count", (req, res) => {
+	res.json({count: users.length})
+})
+
 
 app.listen(PORT, ()=>{
 	console.log("Server running at Port 3003")
