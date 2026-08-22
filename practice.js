16013c1d8eb85fc98abc0f06b6f4aa68f62174e9
Echo-007-sudo <bosspofii@gmail.com>
2026-08-22 23:08:24 +0100

    successfully fetched from memory using helper functions

--- practice.js
+++ practice.js
@@ -3,38 +3,27 @@ const app = express()
 const PORT = 3000
 app.use(express.json())
 
-app.get("/users", async (req, res)=>{
-	try{
-		const result = await fetch("https://jsonplaceholder.typicode.com/users")
-		const data = await result.json()
+const fetchUsers = async () => {
+	const result = await fetch("https://jsonplaceholder.typicode.com/users")
+	const data = await result.json()
+	return data
+}
 
-		res.status(200).json(data)
-	} catch (error) {
-		res.status(500).json({
-			message: "fetching failed",
-			status: false,
-		})
-	}
-})
-
-app.get("/users/:id", async(req, res) => {
+app.get("/", async (req, res) => {
 	try {
-		const id = req.params.id
-		const result = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-		const data = await result.json()
+	const data = await fetchUsers()
 		res.status(200).json(data)
 	} catch (error) {
 		res.status(500).json({
-			message: "fetching failed, server error",
-			status: false
+			message: "fetch failed"
 		})
 	}
 })
 
-app.listen(PORT, () =>{
+
+app.listen(PORT, () => {
 	console.log({
-		message: "server running",
-		port: 3000,
-		health: "healthy"
+		message: "server active",
+		port: 3000
 	})
 })
