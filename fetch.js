9d3d5ceaa55cef94bb04ae49e1057bc580cfecf0
Echo-007-sudo <bosspofii@gmail.com>
2026-08-22 13:18:11 +0100

    practicing fetch

--- fetch.js
+++ fetch.js
@@ -9,10 +9,17 @@ app.get("/", (req, res) =>{
 
 app.get("/externalUsers", 
 	async(req, res) =>{
-		const result = await fetch("https://jsonplaceholder.typicode.com/users")
-		const data = await result.json()
+		try {
+			const result = await fetch("https://jsonplaceholder.typicode.com/users")
+			const data = await result.json()
 
-		res.status(200).json(data)
+			res.status(200).json(data)
+		} catch (error) {
+			res.status(500).json({
+				message: "Failed to fetch",
+				status: false,
+			})
+		}
 })
 
 app.listen(PORT, () =>{
--- loft.js
+++ loft.js
@@ -1,49 +1,33 @@
 const express = require("express")
 const app = express()
-const PORT = 3003
-const users = [
-  { id: 1, name: "Zacksman", role: "Admin", active: true },
-  { id: 2, name: "Bobby Emma", role: "User", active: false },
-  { id: 3, name: "Charlie", role: "Moderator", active: true },
-  { id: 4, name: "Diana", role: "User", active: true }
-];
+const PORT = 3000
 
 app.use(express.json())
 
-app.get("/", (req, res)=>{
-	res.send("Welcome to my test api")
+app.get("/", (req, res) =>{
+	res.status(200).json({ message: "Welcome to my fetching API" })
 })
 
-app.get("/users", (req, res)=>{
-	res.json(users)
-})
+app.get("/externalJson", async(req, res) => {
+	try {
+		const result = await fetch("https://jsonplaceholder.typicode.com/users")
+		const data = await result.json()
 
-app.get("/users/:index/role", (req, res)=>{
-	const index = Number(req.params.index)
-	const user = users[index]
+		res.status(200).json(data)
+	} catch (error) {
+			res.status(500).json({
+				messgae: "Faied to fetch",
+				status: false
+			})
 
-	if (!user){
-		return res.json({message: "User not found"})
 	}
-	res.json({message: user.name})
+
 })
 
-
-app.get("/search", (req,res) => {
-	const role = req.query.role
-	const filtered = users.filter(user => user.role.toLowerCase() === role.toLowerCase())
-
-	if (filtered.length === 0) {
-		return res.status(404).json({ message: "Role not assigned to user" })
-	}
-	res.status(200).json(filtered)
-})
-
-app.get("/count", (req, res) => {
-	res.json({count: users.length})
-})
-
-
-app.listen(PORT, ()=>{
-	console.log("Server running at Port 3003")
+app.listen(PORT, () =>{
+	console.log({
+		message: "Server healthy",
+		status: "Active",
+		port: 3000,
+	})
 })
