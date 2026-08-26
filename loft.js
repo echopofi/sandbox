686fdd5ee872b56253c43380be90ebfb8355ba35
Echo-007-sudo <bosspofii@gmail.com>
2026-08-26 11:48:11 +0100

    added error message helper function

--- loft.js
+++ loft.js
@@ -1,44 +1,62 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
 app.use(express.json())
 
 const fetchData = async(id = "") => {
-	const url = id? 
-	 `https://jsonplaceholder.typicode.com/users/${id}`
-	: "https://jsonplaceholder.typicode.com/users"
-
+	const url = id?
+		`https://jsonplaceholder.typicode.com/users/${id}`
+		: "https://jsonplaceholder.typicode.com/users"
 	const result = await fetch(url)
+
+	if (!result.ok) {
+		throw new Error("fetch failed")
+	}
+
 	const data = await result.json()
+
 	return data
 }
+
+
+function errMessage(res) {
+	return res.status(500).json({
+		message: "failed to fetch",
+		status: false
+	})
+}
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to my revision API"
+    })
+})
+
 
 app.get("/users", async(req, res) => {
 	try {
 		const data = await fetchData()
 		res.status(200).json(data)
 	} catch (error) {
-		res.status(500).json({message: "Failed to fetch",  status: false})
+		errMessage(res)
 	}
 })
 
 app.get("/users/:id", async(req, res) => {
+	const idNumber = req.params.id
+
 	try {
-		const id = req.params.id
-		const data = await fetchData(id)
+		const data = await fetchData(idNumber)
 		res.status(200).json(data)
-	} catch (error) {
-		res.status(500).json({
-			message: "fetch failed",
-			status: false
-		})
-	}
+	} catch (error) { errMessage(res) }
+	
 })
+
+
 
 app.listen(PORT, () => {
 	console.log({
-		message: "server up",
+		message: "Server Running",
 		port: `${PORT}`
 	})
 })
