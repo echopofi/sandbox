e47a9fba9e5bea1653b130f37fbcae4b976f58dc
Echo-007-sudo <bosspofii@gmail.com>
2026-08-26 01:42:57 +0100

    add: implemented get all users seperate from get users by id because ym express version does not support making the id optional

--- practice.js
+++ practice.js
@@ -1,29 +1,54 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
-app.use(express.json())
+const PORT = 3002
 
-const fetchUsers = async () => {
-	const result = await fetch("https://jsonplaceholder.typicode.com/users")
+app.use(express.json())
+const fetchData = async( id = "" ) => {
+	const url = id 
+	? `https://jsonplaceholder.typicode.com/users/${id}`
+	: "https://jsonplaceholder.typicode.com/users"
+	const result = await fetch(url)
 	const data = await result.json()
+
 	return data
 }
 
-app.get("/", async (req, res) => {
+app.get("/users/:id",async (req, res) => {
 	try {
-	const data = await fetchUsers()
+		const idNumber = req.params.id
+		if (idNumber){
+
+			const data = await fetchData(idNumber)
+			return res.status(200).json(data)
+		}
+		const data = await fetchData()
 		res.status(200).json(data)
+
 	} catch (error) {
 		res.status(500).json({
-			message: "fetch failed"
+			message: "failed to fetch",
+			status: false
 		})
 	}
 })
 
 
+app.get("/users", async(req, res) => {
+	try {
+		const data = await fetchData()
+		res.status(200).json(data)
+	} catch (error) {
+		res.status(500).json({
+			message: "failedto fetch",
+			status: false
+		})
+	}
+})
+
 app.listen(PORT, () => {
 	console.log({
-		message: "server active",
-		port: 3000
+		messgage: "server up",
+		port: `${PORT}`,
+		status:"healthy",
 	})
 })
