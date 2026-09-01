727ce9eaab2acd050d82a26d8ce85c40287670ea
Echo-007-sudo <bosspofii@gmail.com>
2026-09-01 11:14:42 +0100

    brain recalling

--- sunday.js
+++ sunday.js
@@ -1,11 +1,10 @@
 const engine = require("express")
 const app = engine()
-const PORT = 3044
+const PORT = 3022
 app.use(engine.json())
 
-
 const dataFetch = async(id = "") => {
-	const url = id 
+	const url = id
 	? `https://jsonplaceholder.typicode.com/users/${id}`
 	: "https://jsonplaceholder.typicode.com/users"
 
@@ -15,39 +14,47 @@ const dataFetch = async(id = "") => {
 	if (!result.ok) {
 		throw new Error({
 			status: false,
-			message: "Internal issue"
+			message: "Result not ok"
 		})
 	}
 
 	return data
 }
 
-function errMsg(res) {
-	return res.status(500).json({
+function errMsg(res){
+	return res.status(200).json({
 		status: false,
 		message: "fetching failed"
 	})
 }
 
-app.get("/",
-	(req, res) => {
-		res.status(200).json({
-			status: true,
-			messgage: "Hello, ",
-		})
+app.get("/", (req, res) => {
+	res.status(200).json({
+		status: true,
+		message: "Afa?"
 	})
+})
 
 app.get("/users", async(req, res) => {
-	try {
+	try{
 		const data = await dataFetch()
 		res.status(200).json(data)
 	} catch (error) {errMsg(res)}
-}) 
+})
+
+app.get("/users/:id", async(req, res) => {
+	try {
+		const idn = req.params.id 
+		const data = await dataFetch(idn)
+		res.status(200).json(data)
+	} catch (error){errMsg(res)}
+	
+})
 
 app.listen(PORT, () => {
 	console.log({
 		status: true,
-		message: "Server running",
+		message: "Server up",
 		port: PORT
 	})
 })
