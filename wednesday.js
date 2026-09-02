2293295017f10046ac8ebb73adaad95828222f4b
Echo-007-sudo <bosspofii@gmail.com>
2026-09-02 21:55:18 +0100

    still needs some polishing

--- wednesday.js
+++ wednesday.js
@@ -1,19 +1,19 @@
 const express = require("express")
 const app = express()
-const PORT = 3005
+const PORT = 3055
 
 app.use(express.json())
 
-const dataFetch = async(id = "") => {
+const fetchData = async(id = "")=> {
 	const url = id
 	? `https://jsonplaceholder.typicode.com/users/${id}`
 	: "https://jsonplaceholder.typicode.com/users"
 
 	const result = await fetch(url)
 	const data = await result.json()
-
+	
 	if (!result.ok) {
-		throw new Error({
+		res.status(500).json({
 			status: false,
 			message: "result not ok"
 		})
@@ -22,34 +22,41 @@ const dataFetch = async(id = "") => {
 	return data
 }
 
-function errorMessage(res){
-	return res.status(500).json({
-		status: false,
-		message: "fetching failed"
+function errMsg(res){
+	return 	res.status(500).json({
+		status: true,
+		message: "fetching failed",
 	})
 }
 
-app.get("/users", async(req, res) => {
-	try {
-		const data = await dataFetch()
-		res.status(200).json(data)
-	} catch (error) {errorMessage(res)}
+
+app.get("/", (req, res) => {
+	res.status(200).json({
+		status: true,
+		message: "welcome, hello"
+	})
 })
 
+app.get("/users", async(req, res) => {
+	try{
+		const data = await fetchData()
+		res.status(200).json(data)
+	} catch (error) {errMsg(res)}
+})
 
 app.get("/users/:id", async(req, res) => {
-	try{
+	try {
 		const idn = req.params.id
-		const data = await dataFetch(idn)
-
+		const data = await fetchData(idn)
 		res.status(200).json(data)
-	} catch (error) {errorMessage(res)}
+
+	} catch (error) {errMsg(res)}
 })
 
 app.listen(PORT, () => {
 	console.log({
 		status: true,
-		message: "Server running",
-		port: PORT,
-	})	
+		message: "Server up",
+		port: PORT
+	})
 })
