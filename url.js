f90f81cad5897d9747b46d061d45b79c231b5755
Echo-007-sudo <bosspofii@gmail.com>
2026-09-03 18:30:28 +0100

    finished inder 15mins

--- url.js
+++ url.js
@@ -1,45 +1,60 @@
 const express = require("express")
 const app = express()
-const PORT = 3000
+const PORT = 3044
 
 app.use(express.json())
 
-const fetchData = async(id = "")=>{
+const dataFetch = async(id = "") => {
 	const url = id
-		? `https://jsonplaceholder.typicode.com/users/${id}`
-		: "https://jsonplaceholder.typicode.com/users"
-	
+	? `https://jsonplaceholder.typicode.com/users/${id}`
+	: "https://jsonplaceholder.typicode.com/users"
+
 	const result = await fetch(url)
+
 	const data = await result.json()
+
+	if (!result.ok){
+		throw new Error("result not ok")
+	}
 
 	return data
 }
 
+function errorMessage(res){
+	return res.status(500).json({
+		status: false,
+		message: "failed to fetch"
+	})
+}
+
 app.get("/", (req, res) => {
 	res.status(200).json({
-		message: "Welcome to my optimized API"
+		status: true,
+		message: "Welcome to my api"
 	})
 })
 
-app.get("/users/:id", async(req, res) => {
+app.get("/users", async(req, res) => {
 	try {
-		const idNumber = req.params.id
-
-		const data = await fetchData(idNumber)
+		const data = await dataFetch()
 		res.status(200).json(data)
-	} catch (error) {
-		res.status(500).json({
-			message: "Failed to fetch",
-			status: false
-		})
-	}
+	} catch (error){errorMessage(res)}
+})
+
+
+app.get("/users/:id", async(req, res) => {
+        try {
+		const idn = req.params.id
+                const data = await dataFetch(idn)
+                res.status(200).json(data)
+        } catch (error){errorMessage(res)}
 })
 
 
 app.listen(PORT, () => {
 	console.log({
-		message: "Server up",
-		port: `${PORT}`,
-		fileName: "url.js"
+		status: true,
+		message: "Server running",
+		port: PORT
 	})
 })
